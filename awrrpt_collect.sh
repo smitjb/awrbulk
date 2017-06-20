1245cf54bf7b176d67271265fbfcdce7f6ae1b52
Jim Smith <jim.smith@uk.bp.com>
2017-06-20 15:07:59 +0100

    Major refactoring

--- awrrpt_collect.sh
+++ awrrpt_collect.sh
@@ -5,6 +5,124 @@
 #
 #
 # =============================================================================
+
+function look_up_password {
+    UNAME=$1
+    SID=$2
+    ssh ramxtxus370.am.ist.bp.com "grep -i :${UNAME}: /var/opt/oracle/pwfile | nawk -F: -v SAD=$SID '\$2==SAD  { print \$4 }';"
+
+}
+function get_job_details {
+    DBDEF=$1
+        
+    grep "^DEF:${DBDEF}" ${PROPSFILE}  | sed 's/:/ /g' 
+    
+}
+
+function get_db_details {
+    $DB=$1
+    
+    con=$(get_con $DB)
+    set $con
+    uname=$1
+    pwd=$2
+    url=$(get_url $DB)
+    set $url
+    host=$1
+    port=$2
+    service=$3
+    
+    echo "$uname $pwd $host $port $sid"
+}
+
+function update_job_details {
+    dbname=$1
+    instance=$2
+    esnap=$3
+    tstamp=$4
+    
+    # lock file
+    # update last and process
+    echo "No error"
+    echo "Updating [$dbname] with [$esnap]"
+    sed  -e " /^DEF:$dbname/ d" -e "/^$/ d" <${PROPSFILE}  >$TMPFILE
+    cat <<-EOCAT >>${TMPFILE}
+DEF:$dbname:$instance:$esnap:$tstamp
+EOCAT
+    mv ${TMPFILE} ${PROPSFILE}
+   # unlock file 
+}
+
+function make_zip_file {
+        dbname=$1
+        tstamp=$2
+        instance=$3
+        LDBNAME=$4
+        zip_file_name=awr_reports_${dbname}_${tstamp}.zip
+        zip -m ${zip_file_name} awr_report_${instance}*.html awr_report_${LDBNAME}*.html
+        
+        echo ${zip_file_name}        
+    
+}
+
+
+function get_con {
+  DB=${1}
+
+grep "^CON:${DB}" ${PROPSFILE}  | sed 's/:/ /g' | while read rectype dbname uname pwd
+do
+  if [ -z ${pwd} -o ${pwd} = "LOOKUP" ];then
+      pwd=$(look_up_password $uname $DB)
+      
+  echo "${uname} ${pwd}"
+done
+
+}
+function get_url {
+  DB=${1}
+
+grep "^URL:${DB}" ${PROPSFILE}  | sed 's/:/ /g' | while read rectype dbname host port service
+do
+  if [ -z ${port} ];then
+      echo ${host}    # tns alias only
+    else
+      echo "${host} ${port} ${service}"  # full econnect syntax.
+  fi
+done
+
+}
+
+function test_connection {
+    U=$1
+    P=$2
+    L=$3
+ set -x
+ echo "[${TNS_ADMIN}]"
+ cat ${TNS_ADMIN}/sqlnet.ora
+ sqlplus -L ${U}/${P}@${L} <<EOSQL
+exit 0
+EOSQL
+if [ $? -ne 0 ];then
+        echo "Connection failed"
+        exit 1
+fi
+
+ set +x
+}
+
+
+function get_snapshot_range {
+    sqlplus  ${SQLDEBUGSILENT} ${CRED}@${LOCAL} <<EOSQL
+set termout on heading off feedback off timing off  verify off
+set linesize 200
+set trimout on
+@${SQLDIR}/awrrpt_get_snapshot_range ${1} ${2}
+EOSQL
+}
+
+#
+# MAIN
+#
 THISDIR=$(dirname $0)
 
 if [ -z "${THISDIR}" -o "." == "${THISDIR}" ]; then
@@ -79,53 +197,6 @@ fi
 export SQLDEBUGSILENT
 cd ${SQLDIR}
 
-function get_con {
-  DB=${1}
-
-grep "^CON:${DB}" ${PROPSFILE}  | sed 's/:/ /g' | while read rectype dbname uname pwd
-do
-  echo "${uname}/${pwd}"
-done
-
-}
-function get_url {
-  DB=${1}
-
-grep "^URL:${DB}" ${PROPSFILE}  | sed 's/:/ /g' | while read rectype dbname host port service
-do
-  if [ -z ${port} ];then
-      echo ${host}    # tns alias only
-    else
-      echo "${host}:${port}/${service}"  # full econnect syntax.
-  fi
-done
-
-}
-
-function test_connection {
- set -x
- echo "[${TNS_ADMIN}]"
- cat ${TNS_ADMIN}/sqlnet.ora
- sqlplus -L ${CRED}@${LOCAL} <<EOSQL
-exit 0
-EOSQL
-if [ $? -ne 0 ];then
-        echo "Connection failed"
-        exit 1
-fi
-
- set +x
-}
-
-
-function get_snapshot_range {
-    sqlplus  ${SQLDEBUGSILENT} ${CRED}@${LOCAL} <<EOSQL
-set termout on heading off feedback off timing off  verify off
-set linesize 200
-set trimout on
-@${SQLDIR}/awrrpt_get_snapshot_range ${1} ${2}
-EOSQL
-}
 
 DBLIST=$*
 if [ -z "${DBLIST}" ];then
@@ -134,8 +205,9 @@ fi
 echo "Looping through databases [${DBLIST}] "
 for DBDEF in ${DBLIST}
 do
-grep "^DEF:${DBDEF}" ${PROPSFILE}  | sed 's/:/ /g' | while read rectype dbname instance lastsnap lasttime
-do
+     jobdetails=$( get_job_details ${DBDEF})
+     set ${jobdetails}
+     rectype=$1; dbname=$2; instance=$3; lastsnap=$4; lasttime=$5;
     unset LOCAL
     unset CRED
     if [ ! -d ${REPORTDIR}/${instance} ];then
@@ -144,60 +216,49 @@ do
     cd ${REPORTDIR}/${instance}
     if [ ! -z "${dbname}" ];then
         echo "Processing $dbname in $instance"
-        #set -x
+        set -x
+         details=$(get_db_details $DB)
+         set $details
+         LOCAL=$3:$4/$5
+         UNAME=$1
+         PW=$2 
         ORACLE_SID=$instance
-        LOCAL=$(get_url ${dbname} )
-        CRED=$(get_con ${dbname} )
+        #LOCAL=$(get_url ${dbname} )
+        #CRED=$(get_con ${dbname} )
         LDBNAME=$(echo ${dbname} | tr '[A-Z]' '[a-z]' )
-        export CRED LOCAL
+        export UNAME PW LOCAL
         if [ ! -z ${SQLBULK_DEBUG} ];then
-            echo "[ ${CRED} | ${LOCAL} ]"
-            test_connection
+            echo "[ ${UNAME} | ${PW} | ${LOCAL} ]"
+            test_connection ${UNAME} ${PW}  ${LOCAL}
         fi
-        get_snapshot_range $dbname $lastsnap | while read bsnap esnap dbid sid db instnum ver host
-        do
-            if [ ! -z ${bsnap} ];then
-                tstamp=$( date +%Y%m%d%H%M%S)
-                if [ ! -z ${SQLBULK_DEBUG} ];then
-                    echo "[$bsnap, $esnap, $dbid, $sid, $db,$instnum,$ver, $host ]"
-                fi
-                echo "${BINDIR}/awrrpt_bulk.sh $bsnap $esnap $dbid $sid $db ${CRED}@${LOCAL}"
-                #${BINDIR}/awrrpt_bulk.sh $bsnap $esnap $dbid $sid $db ${CRED}@${LOCAL}
-         #       $      case $OPTION in
-         #b) bsnap=${OPTARG} ;;
-         #e) esnap=${OPTARG} ;;
-         #i) P_DBID=${OPTARG} ;;
-         #s) P_INSTANCE=${OPTARG} ;;
-         #d) P_DBNAME=${OPTARG} ;;
-         #c) CONNECT_STRING=${OPTARG} ;;
-         #S) SUMMARY="Y" ;;
-         #I) INTERVAL="Y" ;;
-                {BINDIR}/awrrpt_bulk.sh -b$bsnap -e$esnap -i$dbid -s$sid -d$db -c${CRED}@${LOCAL} -I
-                if [ $? -ne 0 ];then
-                    # report error
-                    echo "Error"
-                else
-                    if [ "${RECYCLE_SNAPSHOTS}" = "N" ];then
+        range=$(get_snapshot_range $dbname $lastsnap) 
+        set $range
+        bsnap=$1;esnap=$2;dbid=$3; sid=$4; db=$5; instnum=$6; ver=$7; host=$8;
+        if [ ! -z ${bsnap} ];then
+            tstamp=$( date +%Y%m%d%H%M%S)
+            if [ ! -z ${SQLBULK_DEBUG} ];then
+                echo "[$bsnap, $esnap, $dbid, $sid, $db,$instnum,$ver, $host ]"
+            fi
+            echo "${BINDIR}/awrrpt_bulk.sh $bsnap $esnap $dbid $sid $db ${UNAME}@${LOCAL}"
+            ${BINDIR}/awrrpt_bulk.sh -b$bsnap -e$esnap -i$dbid -s$sid -d$db -c${UNAME}/${PW}@${LOCAL} -I
+            if [ $? -ne 0 ];then
+                # report error
+                echo "Error"
+            else
+                if [ "${RECYCLE_SNAPSHOTS}" = "N" ];then
                     # update last and process
-                    echo "No error"
-                    echo "Updating [$dbname] with [$esnap]"
-                    sed  -e " /^DEF:$dbname/ d" -e "/^$/ d" <${PROPSFILE}  >$TMPFILE
-                    cat <<-EOCAT >>${TMPFILE}
-DEF:$dbname:$instance:$esnap:$tstamp
-EOCAT
-                    mv ${TMPFILE} ${PROPSFILE}
-                    fi
-                    zip -m awr_reports_${dbname}_${tstamp}.zip awr_report_${instance}*.html awr_report_${LDBNAME}*.html
-                    if [ ! -d ${REPORT_LOCATION}/${dbname} ];then
-                        mkdir ${REPORT_LOCATION}/${dbname}
-                    fi
-                    mv  awr_reports_${dbname}_${tstamp}.zip  ${REPORT_LOCATION}/${dbname}
+                    update_job_details  $dbname $instance $esnap $tstamp
                 fi
-
+                zipfile=$(make_zip_file ${dbname} ${tstamp} ${instance} ${LDBNAME})
+                #zip -m awr_reports_${dbname}_${tstamp}.zip awr_report_${instance}*.html awr_report_${LDBNAME}*.html
+                if [ ! -d ${REPORT_LOCATION}/${dbname} ];then
+                    mkdir ${REPORT_LOCATION}/${dbname}
+                fi
+                mv ${zipfile} ${REPORT_LOCATION}/${dbname}
+                #mv  awr_reports_${dbname}_${tstamp}.zip  ${REPORT_LOCATION}/${dbname}
             fi
 
-        done
+        fi
     fi
 cd ${THISDIR}
-done
 done
