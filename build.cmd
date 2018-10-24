c14518ae4e69ba7ce35c41960c90961d1d1e4eb4
Jim Smith <jim.smith@uk.bp.com>
2018-10-24 14:17:10 +0100

    Changed atifactory location

--- build.cmd
+++ build.cmd
@@ -19,7 +19,7 @@ echo Creating new zip file %PROJNAME%_%LABEL%.zip
 zip -r %PROJNAME%_%LABEL%.zip . -x backup/*
 rem 4   put it in artifactory
 set AFURL="http://dml.bpweb.bp.com:8088/artifactory/bp-aesdba-snapshot-local/"
-set AFPATH=utilities/internal/%PROJNAME%
+set AFPATH=utilities/unsupported/%PROJNAME%
 
 rem curl -u%USERNAME% -T  "http://dml.bpweb.bp.com:8088/artifactory/bp-test-cbafdba-generic/%AFPATH%/%PROJNAME%_%LABEL%.zip"
 powershell Invoke-RestMethod -uri "%AFURL%%AFPATH%/%PROJNAME%_%LABEL%.zip"  -Method Put -InFile %PROJNAME%_%LABEL%.zip -Credential %USERNAME% -ContentType "multipart/form-data"
